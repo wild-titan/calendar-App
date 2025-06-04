b66241723f361ee60a9fd8c4cc754ac795cddf48
sowa <sowa.sowa.sowa2007@gmail.com>
2025-06-04 21:05:43 +0900

    add manifest.json service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const cacheName = "v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/java.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
